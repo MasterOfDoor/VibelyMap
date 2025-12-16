0432bf52358a74cc77f51fa69ea30e7fcd20ac1e
MasterOfDoor <ahmet64hant@gmail.com>
2025-12-16 20:48:20 +0300

    Fix: Add ReactNode import to layout.tsx to fix TypeScript build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ReactNode } from "react";
 import "./globals.css";
 import "../style.css";
 import "./utils/suppressConsoleErrors";
